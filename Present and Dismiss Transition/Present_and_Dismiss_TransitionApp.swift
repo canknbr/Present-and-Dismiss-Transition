58257de8a89514516285a3a1312425f1571051da
canknbr <cankanbur1@gmail.com>
2023-04-03 15:56:12 +0300

    Initial Commit

---
+++ Present and Dismiss Transition/Present_and_Dismiss_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Present_and_Dismiss_TransitionApp.swift
+//  Present and Dismiss Transition
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Present_and_Dismiss_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
